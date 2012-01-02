ae27a4e1813c7a4594e701f78fd12ec35cd42b23
ready4god2513 <brandonh@ibethel.org>
2012-01-02 14:34:18 -0800

    Adding validation for forms

--- site_scripts/main.js
+++ site_scripts/main.js
@@ -3,43 +3,55 @@ $(function(){
 	// Drop Down Menu
 	$("ul.sf-menu").superfish();
 	
-	// Form Validations
-	$("form").validate();
 	
 	// Modal Box
 	$("a.enlarge_link").fancybox();
 	
+	
+	// Form Validations
 	$("#signup-form").validate({
-		user_first_name: "required",
-		user_last_name: "required",
-		user_email: {
-			required: true,
-			email: true
-		},
-		user_email2 {
-			required: true,
-			email: true,
-			equalTo: "user_email"
-		},
-		password: {
-			required: true,
-			minlength: 6
-		},
-		password2: {
-			required: true,
-			minlength: 6,
-			equalTo: "password"
+		rules:
+		{
+			user_first_name: "required",
+			user_last_name: "required",
+			user_email: 
+			{
+				required: true,
+				email: true
+			},
+			user_email2:
+			{
+				required: true,
+				email: true,
+				equalTo: "#user_email"
+			},
+			user_password: 
+			{
+				required: true,
+				minlength: 6
+			},
+			user_password2: 
+			{
+				required: true,
+				minlength: 6,
+				equalTo: "#user_password"
+			}
 		}
 	});
 	
 	$("#login-form").validate({
-		user_name: {
-			required: true,
-			email: true
-		},
-		password: {
-			required: true,
-			minlength: 6
+		rules:
+		{
+			user_name: 
+			{
+				required: true,
+				email: true
+			},
+			password: 
+			{
+				required: true,
+				minlength: 6
+			}
 		}
 	});
 });
